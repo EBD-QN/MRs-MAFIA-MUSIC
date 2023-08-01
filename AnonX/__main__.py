869492aa1c601f554cbfe066d7dc454e58d1b93d
EBD-QN <140230639+EBD-QN@users.noreply.github.com>
2023-08-01 21:22:11 +0530

    Update __main__.py

--- AnonX/__main__.py
+++ AnonX/__main__.py
@@ -53,7 +53,7 @@ async def init():
     await Anon.start()
     try:
         await Anon.stream_call(
-            "https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4"
+            "https://te.legra.ph/file/3b2fc225e212606cbc813.mp4"
         )
     except NoActiveGroupCall:
         LOGGER("AnonX").error(
